84c541da953c775323f0417d5ba6320468b361b7
Jevin Sweval <jevinsweval@gmail.com>
2026-05-16 00:35:23 -0400

    less bugs, less suck

--- Src/Modules/zprofline_hashtable.h
+++ Src/Modules/zprofline_hashtable.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#undef NDEBUG
 #include <assert.h>
 
 #include <stdio.h>
@@ -8,12 +7,11 @@
 #include <string.h>
 
 #include "cwisstable.h"
-
 typedef const char *icstr_t;
 
 struct consed_cstr_s {
     size_t hash;
-    size_t len_wo_nul;
+    size_t len_w_nul;
     icstr_t cstr;
 };
 
@@ -23,21 +21,24 @@ static inline void kConsedCstrPolicy_copy(void *dst, const void *src) {
     consed_cstr_t **dccp = (consed_cstr_t **)dst;
     consed_cstr_t **sccp = (consed_cstr_t **)src;
     const size_t src_hash      = (*sccp)->hash;
-    const size_t src_len_wo_nul = (*sccp)->len_wo_nul;
+    const size_t src_len_w_nul = (*sccp)->len_w_nul;
     const char *src_cstr       = (*sccp)->cstr;
 
-    consed_cstr_t *new_ccstr = (consed_cstr_t *)malloc(sizeof(consed_cstr_t) + src_len_wo_nul);
+    consed_cstr_t *new_ccstr = malloc(sizeof(consed_cstr_t) + src_len_w_nul);
+    if (!new_ccstr) {
+        assert(!"malloc failure");
+    }
     new_ccstr->hash      = src_hash;
-    new_ccstr->len_wo_nul = src_len_wo_nul;
+    new_ccstr->len_w_nul = src_len_w_nul;
     char *new_cstr       = (char *)((uintptr_t)new_ccstr + sizeof(consed_cstr_t));
-    memcpy(new_cstr, src_cstr, src_len_wo_nul);
+    memcpy(new_cstr, src_cstr, src_len_w_nul);
     new_ccstr->cstr = new_cstr;
     *dccp           = new_ccstr;
 }
 
 static inline void kConsedCstrPolicy_dtor(void *val) {
-    consed_cstr_t *ccstrp = (consed_cstr_t *)val;
-    free((void *)ccstrp);
+    consed_cstr_t *ccstr = *(consed_cstr_t **)val;
+    free(ccstr);
 }
 
 static inline size_t kConsedCstrPolicy_hash(const void *val) {
@@ -46,28 +47,26 @@ static inline size_t kConsedCstrPolicy_hash(const void *val) {
         return ccstr->hash;
     }
     CWISS_FxHash_State state = CWISS_AbslHash_kInit;
-    CWISS_FxHash_Write(&state, &ccstr->len_wo_nul, sizeof(ccstr->len_wo_nul));
-    CWISS_FxHash_Write(&state, ccstr->cstr, ccstr->len_wo_nul);
-    ccstr->hash = state;
-    return state;
+    CWISS_FxHash_Write(&state, &ccstr->len_w_nul, sizeof(ccstr->len_w_nul));
+    CWISS_FxHash_Write(&state, ccstr->cstr, ccstr->len_w_nul);
+    CWISS_FxHash_Finish(&state);
+    ccstr->hash = CWISS_FxHash_Finish(&state);;
+    return ccstr->hash;
 }
 
 static inline bool kConsedCstrPolicy_eq(const void *a, const void *b) {
     consed_cstr_t **acc = (consed_cstr_t **)a;
     consed_cstr_t **bcc = (consed_cstr_t **)b;
-    if (acc == bcc) {
-        return true;
-    }
     if (*acc == *bcc) {
         return true;
     }
     if ((*acc)->hash != (*bcc)->hash) {
         return false;
     }
-    if ((*acc)->len_wo_nul != (*bcc)->len_wo_nul) {
+    if ((*acc)->len_w_nul != (*bcc)->len_w_nul) {
         return false;
     }
-    int memcmp_res = memcmp((*acc)->cstr, (*bcc)->cstr, (*acc)->len_wo_nul);
+    int memcmp_res = memcmp((*acc)->cstr, (*bcc)->cstr, (*acc)->len_w_nul);
     if (!memcmp_res) {
         return true;
     } else {
@@ -84,10 +83,10 @@ CWISS_DECLARE_HASHSET_WITH(ConsedCstrSet, consed_cstr_t *, kConsedCstrPolicy);
 
 static inline size_t ConsedCstrSet_cstr_hash(const char *self) {
     CWISS_FxHash_State state = CWISS_AbslHash_kInit;
-    const size_t len_wo_nul   = strlen(self);
-    CWISS_FxHash_Write(&state, &len_wo_nul, sizeof(len_wo_nul));
-    CWISS_FxHash_Write(&state, self, len_wo_nul);
-    return state;
+    const size_t len_w_nul   = strlen(self) + 1;
+    CWISS_FxHash_Write(&state, &len_w_nul, sizeof(len_w_nul));
+    CWISS_FxHash_Write(&state, self, len_w_nul - 1);
+    return CWISS_FxHash_Finish(&state);
 }
 
 static inline bool ConsedCstrSet_cstr_eq(const char *self, consed_cstr_t *const *that) {
@@ -98,8 +97,8 @@ CWISS_DECLARE_LOOKUP_NAMED(ConsedCstrSet, cstr, char);
 
 static inline consed_cstr_t *make_consd_cstr(const char *cstr) {
     const size_t len      = strlen(cstr);
-    consed_cstr_t *ccstrp = (consed_cstr_t *)malloc(sizeof(consed_cstr_t) + len + 1);
-    ccstrp->len_wo_nul = len;
+    consed_cstr_t *ccstrp = malloc(sizeof(consed_cstr_t) + len + 1);
+    ccstrp->len_w_nul = len + 1;
     char *cstr_copy   = (char *)((uintptr_t)ccstrp + sizeof(consed_cstr_t));
     memcpy(cstr_copy, cstr, len + 1);
     ccstrp->cstr = cstr_copy;
@@ -109,13 +108,14 @@ static inline consed_cstr_t *make_consd_cstr(const char *cstr) {
 }
 
 static inline icstr_t inter_string_to_set(ConsedCstrSet *set, const char *cstr) {
+    const char *interned_cstr = NULL;
     consed_cstr_t *ccstr      = NULL;
     ConsedCstrSet_Insert ins  = ConsedCstrSet_deferred_insert_by_cstr(set, cstr);
     consed_cstr_t **ccstrp    = ConsedCstrSet_Iter_get(&ins.iter);
     if (ins.inserted) {
         ccstr                  = make_consd_cstr(cstr);
-        const size_t len_wo_nul = strlen(cstr);
-        memcpy((char *)ccstr->cstr, cstr, len_wo_nul + 1);
+        const size_t len_w_nul = strlen(cstr) + 1;
+        memcpy((char *)ccstr->cstr, cstr, len_w_nul);
         ccstr->hash = ConsedCstrSet_cstr_hash(ccstr->cstr);
         *ccstrp     = ccstr;
     } else {
@@ -134,107 +134,16 @@ static inline icstr_t inter_string(const char *cstr) {
     return inter_string_to_set(&global_string_interning_set, cstr);
 }
 
-__attribute__((destructor)) static void deinit_string_interning_set(void) {
-    ConsedCstrSet_destroy(&global_string_interning_set);
-}
-
 struct funcstat_s {
-    icstr_t name;
-    icstr_t filename;
-    icstr_t caller;
-    zlong flineno;
-    zlong lineno;
-    zlong hitcnt;
+    // layout struct so hashed contents are contiguous
+    const icstr_t name;
+    const icstr_t filename;
+    const icstr_t caller;
+    const zlong flineno;
+    const zlong lineno;
+    const int tp;
+    zlong num_execs;
     double ns;
-    int tp;
 };
 
 typedef struct funcstat_s funcstat_t;
-
-static inline void kFuncstatPolicy_copy(void *dst, const void *src) {
-    consed_cstr_t **dccp = (consed_cstr_t **)dst;
-    consed_cstr_t **sccp = (consed_cstr_t **)src;
-    const size_t src_hash      = (*sccp)->hash;
-    const size_t src_len_wo_nul = (*sccp)->len_wo_nul;
-    const char *src_cstr       = (*sccp)->cstr;
-
-    consed_cstr_t *new_ccstr = (consed_cstr_t *)malloc(sizeof(consed_cstr_t) + src_len_wo_nul);
-    new_ccstr->hash      = src_hash;
-    new_ccstr->len_wo_nul = src_len_wo_nul;
-    char *new_cstr       = (char *)((uintptr_t)new_ccstr + sizeof(consed_cstr_t));
-    memcpy(new_cstr, src_cstr, src_len_wo_nul);
-    new_ccstr->cstr = new_cstr;
-    *dccp           = new_ccstr;
-}
-
-static inline void kFuncstatPolicy_dtor(void *val) {
-    consed_cstr_t *ccstrp = (consed_cstr_t *)val;
-    free((void *)ccstrp);
-}
-
-static inline size_t kFuncstatPolicy_hash(const void *val) {
-    consed_cstr_t *ccstr = *(consed_cstr_t **)val;
-    if (ccstr->hash != CWISS_AbslHash_kInit) {
-        return ccstr->hash;
-    }
-    CWISS_FxHash_State state = CWISS_AbslHash_kInit;
-    CWISS_FxHash_Write(&state, &ccstr->len_wo_nul, sizeof(ccstr->len_wo_nul));
-    CWISS_FxHash_Write(&state, ccstr->cstr, ccstr->len_wo_nul);
-    ccstr->hash = state;
-    return state;
-}
-
-static inline bool kFuncstatPolicy_eq(const void *a, const void *b) {
-    consed_cstr_t **acc = (consed_cstr_t **)a;
-    consed_cstr_t **bcc = (consed_cstr_t **)b;
-    if (acc == bcc) {
-        return true;
-    }
-    if (*acc == *bcc) {
-        return true;
-    }
-    if ((*acc)->hash != (*bcc)->hash) {
-        return false;
-    }
-    if ((*acc)->len_wo_nul != (*bcc)->len_wo_nul) {
-        return false;
-    }
-    int memcmp_res = memcmp((*acc)->cstr, (*bcc)->cstr, (*acc)->len_wo_nul);
-    if (!memcmp_res) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-CWISS_DECLARE_NODE_SET_POLICY(kFuncstatPolicy, funcstat_t *,
-                              (obj_copy, kFuncstatPolicy_copy),
-                              (obj_dtor, kFuncstatPolicy_dtor),
-                              (key_hash, kFuncstatPolicy_hash),
-                              (key_eq, kFuncstatPolicy_eq));
-
-CWISS_DECLARE_HASHSET_WITH(FuncstatSet, funcstat_t *, kFuncstatPolicy);
-
-extern FuncstatSet global_funcstat_set;
-
-__attribute__((constructor)) static void init_funcstat_set(void) {
-    global_funcstat_set = FuncstatSet_new(0);
-}
-
-__attribute__((destructor)) static void deinit_funcstat_set(void) {
-    FuncstatSet_destroy(&global_funcstat_set);
-}
-
-static inline size_t FuncstatSet_cstr_hash(const char *self) {
-    CWISS_FxHash_State state = CWISS_AbslHash_kInit;
-    const size_t len_wo_nul   = strlen(self);
-    CWISS_FxHash_Write(&state, &len_wo_nul, sizeof(len_wo_nul));
-    CWISS_FxHash_Write(&state, self, len_wo_nul);
-    return state;
-}
-
-static inline bool FuncstatSet_cstr_eq(const char *self, funcstat_t *const *that) {
-    return !strcmp(self, (*that)->filename);
-}
-
-CWISS_DECLARE_LOOKUP_NAMED(FuncstatSet, cstr, char);
